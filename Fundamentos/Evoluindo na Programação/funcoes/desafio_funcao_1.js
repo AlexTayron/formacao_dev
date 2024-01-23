61da65c8bd127f82680f3cf20bae643100455ff4
Alex Tayron <alextayron@gmail.com>
2024-01-23 16:51:52 -0300

    atividade função

--- Fundamentos/Evoluindo na Programação/funcoes/desafio_funcao_1.js
+++ Fundamentos/Evoluindo na Programação/funcoes/desafio_funcao_1.js
@@ -1,2 +1,15 @@
 //funcao: 3 Params => mediaSimples => console.log 
 //funcao: 3 Params => mediaPonderada => console.log 
+
+function mediaSimples(nota1, nota2, nota3){
+    const media = (nota1 + nota2 + nota3) / 3
+    console.log(`A média simples é ${media}`)
+}
+
+function mediaPonderada(nota1, nota2, nota3){
+    const media = (nota1 + 2 * nota2 + 3 * nota3) / 6
+    console.log(`A média ponderada é ${media}`)
+}
+
+mediaSimples(6, 7, 8)
+mediaPonderada(6, 7, 8)
