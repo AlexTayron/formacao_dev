073b45e7fbadb06e601df0b21754c834716398b3
Alex Tayron <alextayron@gmail.com>
2024-01-23 16:39:50 -0300

    funções

---
+++ Fundamentos/Evoluindo na Programação/funcoes/comm_uso_funcoes_2.js
@@ -0,0 +1,19 @@
+function exibirData(dia, mes, ano){
+    console.log(`Dia: ${dia}`)
+    console.log(`Mês: ${mes}`)
+    console.log(`Ano: ${ano}`)
+    console.log(`${dia} de ${mes} de ${ano}`)
+}
+
+const d1 = 7
+const m1 = 'Maio'
+const a1 = 2022
+
+
+const d2 = 24
+const m2 = 'Dezembro'
+const a2 = 2024
+
+exibirData(7, 'maio', 2022)
+exibirData(d1, m1, a1)
+exibirData(d2, m2, a2)
---
+++ Fundamentos/Evoluindo na Programação/funcoes/desafio_funcao_1.js
@@ -0,0 +1,2 @@
+//funcao: 3 Params => mediaSimples => console.log 
+//funcao: 3 Params => mediaPonderada => console.log 
